86f33ceba6a708ae59f3788d85ad708d91e2c5da
davorpeu <davorpeu@gmai.com>
2021-08-06 14:01:13 +0000

    created mock hero list

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent implements OnInit {
+
+  constructor() {
+
+    
+
+   }
+
+  ngOnInit(): void {
+
+    
+  }
+
+}
+
+export interface Hero {
+  id: number;
+  name: string;
+}
